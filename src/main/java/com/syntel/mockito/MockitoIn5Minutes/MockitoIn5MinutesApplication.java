b473d15b0bbe5bba9dbc014718f31b424a2e323d
Manoj kumar Lekkalapudi <manojkumarlekkalapudi@Manojs-MacBook-Pro.local>
2018-01-15 20:42:33 -0500

    Project Template creation commit

---
+++ src/main/java/com/syntel/mockito/MockitoIn5Minutes/MockitoIn5MinutesApplication.java
@@ -0,0 +1,12 @@
+package com.syntel.mockito.MockitoIn5Minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoIn5MinutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoIn5MinutesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/syntel/mockito/MockitoIn5Minutes/MockitoIn5MinutesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.syntel.mockito.MockitoIn5Minutes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockitoIn5MinutesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
