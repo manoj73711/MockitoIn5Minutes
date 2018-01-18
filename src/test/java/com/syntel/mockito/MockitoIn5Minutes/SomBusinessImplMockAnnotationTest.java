70da2df4c65b37eeef0276aa4c035866e020a117
manojkumarlekkalapudi <manojkumarlekkalapudi@192.168.0.14>
2018-01-17 20:00:34 -0500

    couple of classes are renamed as per their functionality

--- src/test/java/com/syntel/mockito/MockitoIn5Minutes/SomBusinessImplMockAnnotationTest.java
+++ src/test/java/com/syntel/mockito/MockitoIn5Minutes/SomBusinessImplMockAnnotationTest.java
@@ -1,27 +1,38 @@
 package com.syntel.mockito.MockitoIn5Minutes;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SomBusinessImplMockAnnotationTest {
+	
+	@Mock
+	DataService dataService;
+	@InjectMocks
+	SomeBuisinessImpl b;
+
 
 	@Test
-	public void 	returnHighestNumber() {
-		SomeBuisinessImpl b=new SomeBuisinessImpl(new DataStub());
-		int result=b.returnHighestNumber();
-		assertEquals(25,result);
+	public void 	returnHighestNumber_3values() {
+		when(dataService.returnData()).thenReturn(new int[] {2,3,5});
+		assertEquals(5,b.returnHighestNumber());
+	}
+	@Test
+	public void 	returnHighestNumber_1value() {
+		when(dataService.returnData()).thenReturn(new int[] {2});
+		assertEquals(2,b.returnHighestNumber());
+	}
+	@Test
+	public void 	returnHighestNumber_0value() {
+		when(dataService.returnData()).thenReturn(new int[] {});
+		assertEquals(Integer.MIN_VALUE,b.returnHighestNumber());
 	}
 	
-	class DataStub implements DataService{
-
-		@Override
-		public int[] returnData() {
-			// TODO Auto-generated method stub
-			return new int[] {25,2,3};
-		}
-		
-		
-	}
 
 }
